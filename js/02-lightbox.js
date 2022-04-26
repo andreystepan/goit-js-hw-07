665fcc174df6d24b83c9bd64adde7625d792306b
andreystepan <92333697+andreystepan@users.noreply.github.com>
2022-04-26 21:06:08 +0300

    task-07(2)

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,25 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const imageConteiner = document.querySelector('.gallery');
+const imageMarkup = creatGalleryItemsMarkup(galleryItems);
+
+
+imageConteiner.insertAdjacentHTML('beforeend', imageMarkup);
+
+
+function creatGalleryItemsMarkup(items) {
+    return items.map(({ preview, original, description }) => {
+        return `
+        <a class="gallery__item" href="${original}">
+    <img class = "gallery__image" src="${preview}"
+    alt = "${description}" /></a>
+  `
+    }).join('');
+    
+}
+
+
+const lightbox = new SimpleLightbox('.gallery a', { captionsData: "alt", captionDelay: 250, });
+
+
